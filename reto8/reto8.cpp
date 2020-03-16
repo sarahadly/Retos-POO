c25476cded83c9e64342177a00829028ecc9da81
sarahadly <sarah.adly7@gmail.com>
2020-03-16 05:34:26 +0000

    subi reto 8

--- reto8/reto8.cpp
+++ reto8/reto8.cpp
@@ -1,48 +1,57 @@
 #include<iostream>
 #include<vector>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-vector<int> solution(int N, vector<int> A){
-     vector<int>cont;
+vector<int> solution(vector<int> A, int N){
 
-      for(int i=0;i<N;i++){
-        cont.push_back(0);
+    vector<int> contador;
+    int mv;
+
+    for(int i=0; i<N; i++){
+      contador.push_back(0);
     }
 
-      for(int k=0;k<A.size();k++){
-          if(A[k]<=N){
-            cont[A[k]-1]++;
-         }
-         else
-         {
-             if (A[k]=N+1){
+    for(int i=0; i<A.size(); i++){
+      if(A[i]<=N){
+          contador[A[i]-1]++;
+      }else{
+          mv=*max_element(contador.begin(), contador.end());
 
-             }
-         }
-        }
-     return cont;
+          for(int i=0; i<contador.size(); i++){
+              contador[i]=mv;
+          }
+       }
+    }
+
+    return contador;
 }
-int main()
-{
-     int numel;
-     int vari;
-     int N;
-     vector<int> res;
-     cout <<"Ingrese cantidad de contadores: ";
-     cin>>N;
-     
-     cout<<"brother inserte cantidad de datos: ";
-     cin>>numel;
-     
-     vector<int> A;
-     for(int i=0;i<numel;i++){
-        cout<<"parcero inserte el numero"<<i+1<< ": ";
-        cin>>vari;
-        A.push_back(vari);
-    }
-    res = solution(N,A);
-    for(int i=0;i<res.size();i++){
-        cout<<res[i]<< *max_element(A.begin(), A.end())<<",";
-    }
+
+int main(){
+  vector<int> A, res;
+  int N, l, elem;
+
+  cout<<"Cantidad de contadores: ";
+  cin>>N;
+
+  cout<<"Tamaño de array: ";
+  cin>>l;
+
+  for(int i=0; i<l; i++){
+      cout<<"Index "<<i<<" es: ";
+      cin>>elem;
+      A.push_back(elem);
+  }
+
+
+  res=solution(A, N);
+
+  cout<<"Contador final es: ";
+
+  for(int i=0; i<res.size(); i++){
+      cout<<res[i];
+  }
+
+  cout<<endl;
+
 }
